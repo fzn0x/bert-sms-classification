c101720257388f48fdb01fd3ac007ee82d1374da
fzn0x <developerfauzan@gmail.com>
2025-04-09 20:26:42 +0700

    feat: add training logs, models and predictions

--- scripts/predict.py
+++ scripts/predict.py
@@ -17,9 +17,21 @@ def model_predict(text: str):
     return 'SPAM' if prediction == 1 else 'HAM'
 
 def predict():
-    text = "I'm gonna be home soon"
+    text = "Hello, do you know with this crypto you can be rich? contact us in 88888"
     predicted_label = model_predict(text)
-    print(f"Predicted class: {predicted_label}")
+    print(f"1. Predicted class: {predicted_label}") # EXPECT: SPAM
+
+    text = "Help me richard!"
+    predicted_label = model_predict(text)
+    print(f"2. Predicted class: {predicted_label}") # EXPECT: HAM
+
+    text = "You can buy loopstation for 100$, try buyloopstation.com"
+    predicted_label = model_predict(text)
+    print(f"3. Predicted class: {predicted_label}") # EXPECT: SPAM
+
+    text = "Mate, I try to contact your phone, where are you?"
+    predicted_label = model_predict(text)
+    print(f"4. Predicted class: {predicted_label}") # EXPECT: HAM
 
 if __name__ == "__main__":
     predict()
